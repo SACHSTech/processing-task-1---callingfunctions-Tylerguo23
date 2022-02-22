826aa0108eb85829b5ac9e559a59f28b37446722
jiayetguo <jiayeguo@gmail.com>
2022-02-22 09:45:30 -0500

    Final

--- Sketch.java
+++ Sketch.java
@@ -18,69 +18,69 @@ public class Sketch extends PApplet {
   }
 
   public void draw() {
-	//house base
-  fill(255,255,204);
-  rect(200, 300, 400, 300);
-  triangle(200, 300, 400, 200, 600, 300);
-  
-  //grass
-  fill(51, 204, 51);
-  rect(0, 580, 800, 20);
-  
-  //door
-  fill(255, 204, 102);
-  rect(360, 450, 80, 130);
+    //house base
+    fill(255,255,204);
+    rect(200, 300, 400, 300);
+    triangle(200, 300, 400, 200, 600, 300);
+    
+    //grass
+    fill(51, 204, 51);
+    rect(0, 580, 800, 20);
+    
+    //door
+    fill(255, 204, 102);
+    rect(360, 450, 80, 130);
 
-  //windows
-  fill (51, 204, 255);
-  rect(250, 350, 50, 50);
-  rect(500, 350, 50, 50);
-  
-  //yellow (sun and doorknob)
-  fill(255, 255, 0);
-  ellipse(100, 100, 100, 100);
-  ellipse(420, 520, 10, 10);
-  
-  //window lines
-  line(275, 350, 275, 400);
-  line(250, 375, 300, 375);
-  line(525, 350, 525, 400);
-  line(500, 375, 550, 375);
+    //windows
+    fill (51, 204, 255);
+    rect(250, 350, 50, 50);
+    rect(500, 350, 50, 50);
+    
+    //yellow (sun and doorknob)
+    fill(255, 255, 0);
+    ellipse(100, 100, 100, 100);
+    ellipse(420, 520, 10, 10);
+    
+    //window lines
+    line(275, 350, 275, 400);
+    line(250, 375, 300, 375);
+    line(525, 350, 525, 400);
+    line(500, 375, 550, 375);
 
-  //flower stems
-  stroke(51, 153, 51);
-  strokeWeight(4);
-  line(250, 580, 250, 520);
-  line(325, 580, 325, 520);
-  line(475, 580, 475, 520);
-  line(550, 580, 550, 520);
-  
-  //flowers
-  stroke(0);
-  strokeWeight(0);
+    //flower stems
+    stroke(51, 153, 51);
+    strokeWeight(4);
+    line(250, 580, 250, 520);
+    line(325, 580, 325, 520);
+    line(475, 580, 475, 520);
+    line(550, 580, 550, 520);
+    
+    //flowers
+    stroke(0);
+    strokeWeight(0);
 
-  fill(255, 153, 0);
-  ellipse(250, 520, 50, 20);
-  ellipse(250, 520, 20, 50);
+    fill(255, 153, 0);
+    ellipse(250, 520, 50, 20);
+    ellipse(250, 520, 20, 50);
+    
+    ellipse(550, 520, 50, 20);
+    ellipse(550, 520, 20, 50);
 
-  ellipse(550, 520, 50, 20);
-  ellipse(550, 520, 20, 50);
+    fill(255, 102, 255);
+    ellipse(325, 520, 50, 20);
+    ellipse(325, 520, 20, 50);
 
-  fill(255, 102, 255);
-  ellipse(325, 520, 50, 20);
-  ellipse(325, 520, 20, 50);
-
-  ellipse(475, 520, 50, 20);
-  ellipse(475, 520, 20, 50);
+    ellipse(475, 520, 50, 20);
+    ellipse(475, 520, 20, 50);
 
 
-  //flower centers
-  strokeWeight(1);
-  fill(255, 255, 0);
-  ellipse(250, 520, 20, 20);
-  ellipse(325, 520, 20, 20);
-  ellipse(475, 520, 20, 20);
-  ellipse(550, 520, 20, 20);
+    //flower centers
+    strokeWeight(1);
+    fill(255, 255, 0);
+    ellipse(250, 520, 20, 20);
+    ellipse(325, 520, 20, 20);
+    ellipse(475, 520, 20, 20);
+    ellipse(550, 520, 20, 20);
 
   }
   
